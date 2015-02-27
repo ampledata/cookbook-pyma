e088b0751ef9e7efb37c5565e30ff0a90e145b23
Greg Albrecht <gba@onbeep.com>
2015-02-26 16:28:17 -0800

    adding blacklist

---
+++ recipes/_blacklist.rb
@@ -0,0 +1,11 @@
+#
+# Recipe:: _blacklist
+# Cookbook:: pymultimonaprs
+# Author:: Greg Albrecht W2GMD <gba@gregalbrecht.com>
+# Copyright:: Copyright 2015 OnBeep, Inc.
+# License:: Apache License, Version 2.0
+# Source:: https://github.com/ampledata/cookbook-pymultimonaprs
+#
+
+
+cookbook_file '/etc/modprobe.d/raspi-blacklist.conf'
