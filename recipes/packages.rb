752055823505901f30cb80c8a7ba7843e3068db7
Greg Albrecht <gba@onbeep.com>
2016-11-03 14:19:28 -0700

    rename pyma -> pymma

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 #
-#
-# Recipe:: rtl-sdr
+# Recipe:: packages
 # Cookbook:: pyma
 # Author:: Greg Albrecht W2GMD <oss@undef.net>
 # Copyright:: Copyright 2016 Orion Labs, Inc.
@@ -10,5 +10,7 @@
 #
 
 
-package 'rtl-sdr'
-package 'librtlsdr-dev'
+['qt4-qmake', 'libpulse-dev', 'libx11-dev', 'rtl-sdr',
+    'librtlsdr-dev'].each do |pkg|
+  package pkg
+end
